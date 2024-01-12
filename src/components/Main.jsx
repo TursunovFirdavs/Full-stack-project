0b5baab326de5bbb0697618a1fa03d6d85821762
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-01-13 03:41:15 +0500

    delete article

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,6 +8,7 @@ import { getArticleStart, getArticleSuccess } from '../slice/article';
 
 const Main = () => {
   const { articles, isLoading } = useSelector(state => state.article)
+  const { isLogin, user } = useSelector(state => state.auth)
   const dispatch = useDispatch()
   const navigate = useNavigate()
   console.log(articles);
@@ -15,9 +16,18 @@ const Main = () => {
   const getArticles = async () => {
     dispatch(getArticleStart())
     try {
-      const response = await ArticleService.getArticle() 
+      const response = await ArticleService.getArticle()
       dispatch(getArticleSuccess(response.articles))
       console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const deleteArticle = async (slug) =>{
+    try {
+      await ArticleService.deleteArticle(slug)
+      getArticles()
     } catch (error) {
       console.log(error);
     }
@@ -29,7 +39,7 @@ const Main = () => {
   return (
     <div>
       {
-        isLoading && <Loader/>
+        isLoading && <Loader />
       }
       <div className="album py-5">
         <div>
@@ -42,17 +52,21 @@ const Main = () => {
                     <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em"></text></svg>
 
                     <div className="card-body">
-                      <p title={artc.title} className="card-text">{artc.title.length < 40 ? artc.title : artc.title.slice(0, 40) + '...' }</p>
-                      <p title={artc.description} className="card-text">{artc.description.length < 80 ? artc.description : artc.description.slice(0, 80) + '...' }</p>
+                      <p title={artc.title} className="card-text">{artc.title.length < 40 ? artc.title : artc.title.slice(0, 40) + '...'}</p>
+                      <p title={artc.description} className="card-text">{artc.description.length < 80 ? artc.description : artc.description.slice(0, 80) + '...'}</p>
                     </div>
                     <div className="card-footer d-flex justify-content-between align-items-center">
-                        <div className="btn-group">
-                          <button onClick={() => navigate(`/article/${artc.slug}`)} type="button" className="btn btn-sm btn-outline-success">View</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                          <button type="button" className="btn btn-sm btn-outline-danger">delete</button>
-                        </div>
-                        <small className="text-muted">{artc.author.username}</small>
+                      <div className="btn-group">
+                        <button onClick={() => navigate(`/article/${artc.slug}`)} type="button" className="btn btn-sm btn-outline-success">View</button>
+                        {isLogin && user.username === artc.author.username && (
+                          <>
+                            <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                            <button onClick={() => deleteArticle(artc.slug)} type="button" className="btn btn-sm btn-outline-danger">delete</button>
+                          </>
+                        )}
                       </div>
+                      <small className="text-muted">{artc.author.username}</small>
+                    </div>
                   </div>
                 </div>
               )
